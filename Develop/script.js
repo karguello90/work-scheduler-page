a17023a86dc68380d933d60af0c6fd6a1d8d4f14
karguello90 <karguello90@gmail.com>
2023-02-22 14:41:34 -0500

    creating divs for additonal time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -18,7 +18,9 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  $("#9 textarea").val(localStorage.getItem("9"));
+  $("#10 textarea").val(localStorage.getItem("10"));
+  $("#11 textarea").val(localStorage.getItem("11"));
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
